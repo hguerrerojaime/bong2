053764cd4461741b79ee32bc31c452ad1c064ecf
Humberto Guerrero Jaime <Guerrero.Humberto@principal.com>
2016-10-04 16:48:39 -0500

    update

--- server/system.init.js
+++ server/system.init.js
@@ -77,13 +77,22 @@
   var modules = ['jquery','bootstrap','app'];
     
   function importModules(modules) {
+    
+    
 	  
 	  if (modules.length > 0) {
 		  var module = modules.shift();
-		  
-		  console.log('importing '+module);
+      
+      console.log("MODULE ["+module+"] is loading");
+      
+      var beforeTs = new Date().getTime();
 		  
 		  System.import(module).then(function (m) {
+        
+        var afterTs = new Date().getTime();
+        
+        console.log("MODULE ["+module+"] loaded after ["+(afterTs - beforeTs)+" ms]");
+        
 			  importModules(modules);
 		  })
 		  .catch( function (err) { 
