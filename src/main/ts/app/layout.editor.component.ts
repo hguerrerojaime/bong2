e2c7e2efc5cce3fce3d5e37f715ca5ce458e447c
Humberto Guerrero <hguerrerojaime@gmail.com>
2016-11-06 20:39:00 -0600

    update

--- src/main/ts/app/layout.editor.component.ts
+++ src/main/ts/app/layout.editor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
@@ -26,17 +26,25 @@ import { DragulaService } from 'ng2-dragula/ng2-dragula';
 						        <div>Space</div>
 						        <div>Label</div>
 				    		</div>
-				    		<div class="col-md-4 container" [dragula]='"section-bag"' [dragulaModel]="sectionElements">
-							    <div *ngFor='let section of sectionElements' [innerHtml]='section.name'></div>
+				    		<div class="col-md-4 container" >
+							    <div [dragula]="'section-bag'" [dragulaModel]="[sectionElement]">
+							    	<div class="drag-handle">Section</div>
+							    </div>
+							    <div [dragula]='"section-col-bag"' [dragulaModel]="[columnElement]">
+							    	<div>Section Column</div>
+							    </div>
 				    		</div>
 				    	</div>
 		    		</div-col>
 		    	</div-row>
 		    	<div-row>
-		    		<div class="col-md-12 container" [dragula]='"section-bag"' [dragulaModel]="layout">
+		    		<div #layoutSectionBag class="col-md-12" [dragula]="'section-bag'" [dragulaModel]="layout">
 		    			<div *ngFor='let section of layout'>
-		    				<div>{{section.name}} 
-		    					<command-button icon="fa fa-edit" label="Edit Title"></command-button>
+		    				<div class="drag-handle" > {{ section.name }}  </div>
+		    				<div class="row" [dragula]="'section-col-bag'" [dragulaModel]="section.columns"> 
+		    					<div *ngFor="let col of section.columns; let i = index" class="col-md-{{ 12 / section.columns.length }}">
+		    						Column
+		    					</div>
 		    				</div>
 		    			</div>
 		    		</div>
@@ -45,35 +53,78 @@ import { DragulaService } from 'ng2-dragula/ng2-dragula';
 		    </panel>
 		  
 
-	`
+	`,
+	viewProviders: [ DragulaService ]
 })
 export class LayoutEditorComponent {
 
-	private sectionElements = [
-		{ name:"New Section" }
-	];
+	@ViewChild("layoutSectionBag")
+	private layoutSectionBag:ElementRef;
+
+	private sectionElement = { name:"New Section",columns:[ { components:[] } ] };
+	private columnElement = { components:[] };
+
 
 	private layout:Object[] = [
-		{ name:"Existing Section" }
-
+		{ name:"Existing Section",
+		  columns:[ 
+			{ components:[] },
+			{ components:[] }
+		  ] 
+		}
 	];
+
+
 
 	constructor(private dragulaService: DragulaService) {
 
+
+		dragulaService.setOptions('fields-bag', {
+	    	accepts: (e, target, source) => {
+	    		return source !== target;
+	    	} 
+	    });
 		
 	    dragulaService.setOptions('section-bag', {
-	    	copy: true
+	    	copy: true,
+	    	copySortSource: false,
+	    	moves:(e, container, handle) => {
+	    		
+	    		return handle.className == "drag-handle";
+	    	},
+	    	accepts: (e, target, source) => {
+	    		return source !== this.layoutSectionBag.nativeElement;
+	    	}
 	    });
+
+	    dragulaService.setOptions('section-col-bag', {
+	    	copy: true,
+	    	accepts: (e, target, source, model) => {
+	    		console.log(target.children);
+	    		return target.children.length  <= 3;
+	    	}
+	    });
+
+	    
 
 	    // dragulaService.drop.subscribe((value) => {
 	    //   this.onDrop(value.slice(1));
 	    // });
 	}
 
-	private onDrop(args) {
-		let [e, el] = args;
-
-	    console.log(e);
+	private onOver(args) {
+	    let [el, container, source] = args;
+	    if(container.id!=="content")
+	      el.remove();
+	}
+	  
+	//(0 - bagname, 1 - el, 2 - target, 3 - source, 4 - sibling)
+	private onDrop(value) {
+		console.log(value[2]);
+	    if (value[2] == null) //dragged outside any of the bags
+	      return;
+	    if (value[2].id !== "content" && value[2].id !== value[3].id) //dragged to a container that should not add the element
+	      value[1].remove();
 	}
 
 	save() {
